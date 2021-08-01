c498bc2c279db01cf69454f6cb2f55775e1b3f1c
Rob Morandini <robjmorandini@hotmail.com>
2021-08-01 18:43:12 -0400

    Add for loop, basic user input

---
+++ basics/loop/index.js
@@ -0,0 +1,10 @@
+const app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+            { text: 'Learn Vue' },
+            { text: 'Build cool stuff'},
+            { text: 'Get a job'}
+        ]
+    }
+})
