f500d9c460bb9c53a43124a45fc0874b0e44a3c6
Rob Morandini <robjmorandini@hotmail.com>
2021-08-01 17:59:40 -0400

    Initial commit

---
+++ basics/conditionals/index.js
@@ -0,0 +1,6 @@
+const app3 = new Vue({
+    el: '#app-3',
+    data: {
+        seen: true
+    }
+});
---
+++ basics/text-interpolation/index.js
@@ -0,0 +1,6 @@
+const app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+});
